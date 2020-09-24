f08f2623a27f26f65149729eb77d0e1f95d814ca
TheGreatFez <raul.maldonado83@gmail.com>
2020-09-24 00:00:09 -0500

    Added variable time step. Added simulation while loop to run until max time. Added list output data

--- SimulationTesting.py
+++ SimulationTesting.py
@@ -17,7 +17,6 @@ def Derivatives ( time, state, Planet ):
     
     ders = np.zeros(state.size,dtype='float64')
     ders[0:3] = gravityAccel(state,Planet)
-    print(ders)
     
     return ders
 
@@ -37,7 +36,7 @@ def normalize(v):
        return v
     return v / norm
 
-def rk45 ( time, state, Planet, time_step):
+def rk45 ( time, state, Planet, time_step, time_max):
     # Butcher Tableu for 4th and 5th order
     c = np.array([0, 1/4, 3/8, 12/13, 1, 1/2],dtype='float64')
     A = np.array([[0, 0, 0, 0, 0, 0],
@@ -49,34 +48,51 @@ def rk45 ( time, state, Planet, time_step):
     b1 = np.array([25/216, 0, 1408/2565, 2197/4104, -1/5, 0],dtype='float64')
     b2 = np.array([16/135, 0, 6656/12825, 28561/56430, -9/50, 2/55],dtype='float64')
     
-    k = np.zeros((c.size,state.size),dtype='float64')
-    state_out_4 = state
-    state_out_5 = state
-    for i in range(c.size):
-        if ( i == 0 ):
-            k[i] = Derivatives(time,state, Planet)
-        else:
-            state_sum = state
-            k_sum = np.zeros(state.size,dtype='float64')
-            
-            for j in range(i-1):
-                k_sum += A[i][j]*k[j]
-            
-            state_sum = state + k_sum*time_step
-            
-            k[i] = Derivatives(time + c[i]*time_step,state_sum,Planet)
-            
-            state_out_4 = state_out_4 + time_step*b1[i]*k[i]
-            state_out_5 += time_step*b2[i]*k[i]
+    # Tolerance factor
+    tol = 1*10**-8
+    error = tol*2
     
-    error = state_out_5 - state_out_4
-    print(error)
-    
-    # ToDo: Use error to determine new time step
-    time_step_out = time_step
-    time_out = time + time_step
+    # Time Step Update Max Fractional change
+    max_frac = 5
+    max_time_step = 10 # sec
+    while error > tol:
+        # Initialize k array
+        k = np.zeros((c.size,state.size),dtype='float64')
+        
+        # Initialize 4th order and 5th order state estimations
+        state_out_4 = state
+        state_out_5 = state
+        
+        # Check so time doesn't go past max time
+        time_step = min([time_step,time_max-time])
+        
+        for i in range(c.size):
+            if ( i == 0 ):
+                k[i] = Derivatives(time,state, Planet)
+            else:
+                state_sum = state
+                k_sum = np.zeros(state.size,dtype='float64')
+                
+                for j in range(i-1):
+                    k_sum += A[i][j]*k[j]
+                
+                state_sum = state + k_sum*time_step
+                
+                k[i] = Derivatives(time + c[i]*time_step,state_sum,Planet)
+                
+                state_out_4 = state_out_4 + time_step*b1[i]*k[i]
+                state_out_5 += time_step*b2[i]*k[i]
+        
+        error = max(abs((state_out_5 - state_out_4)/np.linalg.norm(state_out_5)))
+        # ToDo: Use error to determine new time step
+        time_step_out = time_step*min([max_frac,0.9*(tol/error)**(1/4)])
+        time_step_out = min([time_step_out,max_time_step])
+        
+        time_out = time + time_step
         
     return time_out, state_out_4, time_step_out
+    
+    
 
 ##############################################################################
 #                           Main Function                                    #
@@ -100,10 +116,28 @@ state0 = np.array([100, # X
                    10],dtype='float64') # Mass
 
 # Add radius of the Earth
-
 state0[0] += Planet["Radius"]
 
+# Initialize iterative state
+state = state0
+
+# Initiate and set time data
 time_step = 0.1
 time0 = 0
+time_max = 10
+time = time0
 
-(time_out, state_out, h_out) = rk45(0,state0,Planet,time_step)
+# Initiate simulation output
+state_data = []
+state_data.append(state)
+time_data  = []
+time_data.append(time)
+
+while time < time_max:
+    (time, state, time_step) = rk45(time,state,Planet,time_step,time_max)
+    
+    state_data.append(state)
+    time_data.append(time)
+    
+    print(time)
+    
